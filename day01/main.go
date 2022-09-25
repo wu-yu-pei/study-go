f5e3087659e98e82beb5d5c9259f555cbc1cef19
Wuyupei <495174699@qq.com>
2022-09-25 16:31:20 +0800

    day01

---
+++ day01/main.go
@@ -0,0 +1,142 @@
+package main
+
+import (
+	// "fmt"
+	// "strings"
+)
+
+// "bytes"
+// "fmt"
+// "strings"
+// "math"
+
+func foo() {
+
+}
+
+func main() {
+	// 1.打印
+	// fmt.Printf("\"a\": %v\n", "a")
+
+	// 2.变量的定义
+	// var name string = "wuyupei"
+	// var age int = 20
+
+	// var (
+	// 	name   string = "wuyupei"
+	// 	age    int    = 20
+	// 	isLeft bool   = false
+	// )
+
+	// name := "wuyupei"
+	// age := 20
+	// isLeft := true
+
+	// var name, age, isLeft = "wuyupei", 20, true
+
+	// const name string = "wuyupei"
+	// const age int = 20
+	// const isLeft = true
+	// fmt.Printf("name: %v\n", name)
+	// fmt.Printf("age: %v\n", age)
+	// fmt.Printf("isLeft: %v\n", isLeft)
+
+	// 3.数据类型
+	// var a string = "wuyupei"
+	// var b int = 20
+	// var c float32 = 1.00
+	// var d = &a
+
+	// var e = [...]int{1,2,3}
+
+	// fmt.Printf("%T\n", a)
+	// fmt.Printf("%T\n", b)
+	// fmt.Printf("%T\n", c)
+	// fmt.Printf("%T\n", d)
+	// fmt.Printf("%T\n", foo)
+	// fmt.Printf("e: %T\n", e)
+
+	// 数组
+	// 创建一个空数组
+	// var names [10]string
+	// for i := 0; i < 10; i++ {
+	// 	names[i] = "1"
+	// }
+	// fmt.Printf("names: %v\n", names)
+
+	// name := [...]string{"a"}
+	// fmt.Printf("name: %v\n", name)
+
+	//  for 循环
+	// var i int
+	// for i = 0; i < 10; i++ {
+	// 	fmt.Printf("i: %v\n", i)
+	// }
+
+	// var name = "hello word"
+	// fmt.Printf("name: %v\n", name)
+
+	// bool类型  if语句
+
+	// var age bool = true
+
+	// age := 18
+
+	// age = 2
+	// isMan := age >= 18
+	// if isMan {
+	// 	fmt.Printf("是一个成年人")
+	// } else if age == 2 {
+	// 	fmt.Printf("不是一个成年人")
+	// } else {
+	// 	fmt.Printf("AA")
+	// }
+
+	// 数字类型
+	// var a int
+	// var b int16
+	// var c int32
+	// var d int64
+	// fmt.Printf("a: %v %v %T\n", a, math.MaxInt, a)
+	// fmt.Printf("a: %v\n", b)
+	// fmt.Printf("a: %v\n", c)
+	// fmt.Printf("a: %v\n", d)
+
+	// 字符串
+	// var name string = "wuyupei"
+	// var age = "20"
+
+	// name := "wuyupei"
+	// age := "20"
+	// var res = name + age
+	// fmt.Printf("res: name=%sage=%s\n", name, age)
+	// fmt.Printf("res: name=%vage=%v\n", name, age)
+
+	// strings.Join
+	// var res = strings.Join([]string{name, age}, ",")
+	// fmt.Printf("res: %v\n", res)
+
+	// burrer.WriteString
+	// var buffer bytes.Buffer
+	// buffer.WriteString("a")
+	// buffer.WriteString("b")
+	// buffer.WriteString("c")
+	// fmt.Printf("value: %v\n", buffer.String())
+
+	// foo := [10]string{"10", "", "10"}
+	// fmt.Printf("foo: %v\n", foo)
+
+	// name := "wuyupei"
+	// print(name + "\r")
+	// text := "abcdefg hijklmnopq rstuvwxyz"
+	// print(text[12:] + "\n")
+	// print(text + "\n")
+	// res := strings.Split(text, "")
+	// fmt.Printf("res: %v\n", res)
+	// fmt.Printf("res: %T\n", res)
+	// fmt.Printf("res: %s\n", res)
+	// print("----------------------\n")
+	// print(res)
+	// age := 20
+	// print(age)
+}
